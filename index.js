9de1c8ea8718c53fb070691717f409320a2a45d9
Terry Leggett II <terryleggettjr@gmail.com>
2024-12-15 18:17:14 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().startsWith(letters.toLowerCase())
+    })
+}
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.name.toLowerCase() === name.toLowerCase();
+    })
+}
